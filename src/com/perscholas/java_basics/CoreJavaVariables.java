80857b2935dd5075148b2c9f97c36962c22ddd5b
Caleb Standridge <caleb.standridge@csuglobal.edu>
2022-11-16 17:50:56 -0600

    Init commit

---
+++ src/com/perscholas/java_basics/CoreJavaVariables.java
@@ -0,0 +1,18 @@
+package com.perscholas.java_basics;
+/**
+ * 
+ * @author caleb
+ * @classSection 12-2022-JAVA
+ * @instructor Ms. Lewis
+ * @date 11/16/2022
+ * @programOverview using variables to solve small practice problems
+ *
+ */
+public class CoreJavaVariables {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author caleb
+ *
+ */
+module CoreJavaBasics {
+}
